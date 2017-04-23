c02a59feb981e7c3cace073168bed2d2ab592607
Petr Řehoř <rx@rx.cz>
2017-04-24 00:38:51 +0200

    Test only own docker_entry.d files

--- spec/docker/docker_entrypoint_spec.rb
+++ spec/docker/docker_entrypoint_spec.rb
@@ -7,13 +7,7 @@ describe "Docker Entrypoint" do
   end
 
   [
-    "/docker-entrypoint.d/01-lib-messages.sh",
-    "/docker-entrypoint.d/02-lib-wait-for.sh",
     "/docker-entrypoint.d/10-default-command.sh",
-    "/docker-entrypoint.d/20-docker-introspection.sh",
-    "/docker-entrypoint.d/30-environment-certs.sh",
-    "/docker-entrypoint.d/40-server-certs.sh",
-    "/docker-entrypoint.d/90-exec-command.sh",
   ].each do |file|
     describe file(file) do
       it { should be_file }
