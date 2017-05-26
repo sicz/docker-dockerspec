6a8fb5892562b94d5c737b020474b972b7bbee84
Petr Řehoř <rx@rx.cz>
2017-05-26 22:36:59 +0200

    Enhance tests

--- spec/docker/10_docker_image_spec.rb
+++ spec/docker/10_docker_image_spec.rb
@@ -64,3 +64,41 @@ describe "Ruby gems" do
     end
   end
 end
+
+describe "Docker entrypoint file" do
+  context "/docker-entrypoint.sh" do
+    it "has set permissions" do
+      expect(file("/docker-entrypoint.sh")).to exist
+      expect(file("/docker-entrypoint.sh")).to be_executable
+    end
+  end
+  [
+    "/docker-entrypoint.d/01-lib-messages.sh",
+    "/docker-entrypoint.d/02-lib-wait-for.sh",
+    "/docker-entrypoint.d/10-default-command.sh",
+    "/docker-entrypoint.d/20-docker-introspection.sh",
+    "/docker-entrypoint.d/30-environment-certs.sh",
+    "/docker-entrypoint.d/40-server-certs.sh",
+    "/docker-entrypoint.d/90-exec-command.sh",
+  ].each do |file|
+    context file do
+      it "exists" do
+        expect(file(file)).to exist
+        expect(file(file)).to be_readable
+      end
+    end
+  end
+end
+
+describe "Configuration file" do
+  [
+    "/etc/ssl/openssl.cnf",
+  ].each do |file|
+    context file do
+      it "exists" do
+        expect(file(file)).to exist
+        expect(file(file)).to be_readable
+      end
+    end
+  end
+end
