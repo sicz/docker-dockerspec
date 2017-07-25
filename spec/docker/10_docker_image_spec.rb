6b07c64af8347777d9c22c1ca00268b1f54870a9
Petr Řehoř <rx@rx.cz>
2017-07-25 23:11:37 +0200

    Sync tests with sicz/docker-baseimage-alpine
    [skip ci]

--- spec/docker/10_docker_image_spec.rb
+++ spec/docker/10_docker_image_spec.rb
@@ -1,8 +1,30 @@
 # encoding: UTF-8
 require "docker_helper"
 
+# Keep in sync with:
+# - https://github.com/sicz/docker-baseimage-alpine
+# - https://github.com/sicz/docker-baseimage-centos
+# - https://github.com/sicz/docker-dockerspec
+describe "Operating system" do
+  subject do
+    os
+  end
+  it "is Alpine Linux #{ENV["DOCKER_TAG"]}" do
+    expect(subject[:family]).to eq("alpine")
+    expect(subject[:release]).to match(/^#{Regexp.escape(ENV["DOCKER_TAG"])}\./)
+  end
+end
+
 describe "Package" do
   [
+    "bash",
+    "ca-certificates",
+    "curl",
+    "jq",
+    "libressl",
+    "runit",
+    "su-exec",
+    "tini",
     "git",
     "make",
     "openssh-client",
@@ -43,6 +65,47 @@ describe "Ruby gem" do
     context package do
       it "is installed" do
         expect(package(package)).to be_installed.by('gem')
+      end
+    end
+  end
+end
+
+describe "Docker entrypoint file" do
+  context "/docker-entrypoint.sh" do
+    it "is installed" do
+      expect(file("/docker-entrypoint.sh")).to exist
+      expect(file("/docker-entrypoint.sh")).to be_file
+      expect(file("/docker-entrypoint.sh")).to be_executable
+    end
+  end
+  [
+    "/docker-entrypoint.d/01-lib-messages.sh",
+    "/docker-entrypoint.d/02-lib-wait-for.sh",
+    "/docker-entrypoint.d/10-default-command.sh",
+    "/docker-entrypoint.d/20-docker-introspection.sh",
+    "/docker-entrypoint.d/30-environment-certs.sh",
+    "/docker-entrypoint.d/40-server-certs.sh",
+    "/docker-entrypoint.d/90-exec-command.sh",
+  ].each do |file|
+    context file do
+      it "is installed" do
+        expect(file(file)).to exist
+        expect(file(file)).to be_file
+        expect(file(file)).to be_readable
+      end
+    end
+  end
+end
+
+describe "Configuration file" do
+  [
+    "/etc/ssl/openssl.cnf",
+  ].each do |file|
+    context file do
+      it "is installed" do
+        expect(file(file)).to exist
+        expect(file(file)).to be_file
+        expect(file(file)).to be_readable
       end
     end
   end
